e3c786c73e5cc815bfd7aaf94e54686b9eeb6968
sebastian <sebastt.93@gmail.com>
2021-01-10 14:13:54 +1100

    Reach hooks(useState and useEffect)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/components/Footer.scss'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a href="/">Terms</a>
+    <a href="/">Privacy</a>
+    <a href="/">Help contact</a>
+  </footer>
+);
+
+export default Footer;
